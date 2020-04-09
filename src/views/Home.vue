83c844e05f07daaddc72993fa9be5bfc54cb3c78
DARIN NELSON <nelson.darinp@gmail.com>
2020-04-09 16:49:59 -0500

    tryingaxios

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+var axios = require("axios");
+
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      products: []
+    };
+  },
+  created: function () {
+    axios.get("http://localhost:3000/api/products").then(function (response) {
+      this.products = response.data;
+    });
+  },
+  methods: {},
+  computed: {}
+};
+</script>
